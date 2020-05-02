5df8662aef3a535a0cb2893a1e402acda5b4c966
Pabriones <Pabriones@users.noreply.github.com>
2020-05-02 16:40:07 -0700

    added recyclerView, rename app to AD340 Weather, Update RecyclerView margin, create list item layout, update list item background, create forecast repository, display forecast data, Psuedo-randomize forecast data, Display click feedback, Display touch feedback on list items, and Format temperature String.

--- app/src/main/java/com/dark_matter/pabri/ad340/MainActivity.kt
+++ app/src/main/java/com/dark_matter/pabri/ad340/MainActivity.kt
@@ -4,9 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val forecastRepository = ForecastRepository()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,10 +28,27 @@ class MainActivity : AppCompatActivity() {
             if(zipcode.length != 5){
                 Toast.makeText(this, R.string.zipcode_entry_error, Toast.LENGTH_SHORT).show()
             }else{
-                Toast.makeText(this, zipcode, Toast.LENGTH_SHORT).show()
+                forecastRepository.loadForecast(zipcode)
             }
 
             //Toast.makeText(this, "Submitted", Toast.LENGTH_SHORT).show()
         }
+
+        val forecastList: RecyclerView = findViewById(R.id.forecastList)
+        forecastList.layoutManager = LinearLayoutManager(this)
+
+        val dailyForecastAdapter = DailyForecastAdapter(){forecastItem ->
+            val msg = getString(R.string.forecast_clicked_format, forecastItem.temp, forecastItem.description)
+            Toast.makeText(this, msg,Toast.LENGTH_SHORT).show()
+        }
+        forecastList.adapter = dailyForecastAdapter
+
+        val weeklyForecastObserver = Observer<List<DailyForecast>>{foreCastItems ->
+            // update our list adapter
+            dailyForecastAdapter.submitList(foreCastItems)
+        }
+
+        forecastRepository.weeklyForecast.observe(this, weeklyForecastObserver)
+
     }
 }
